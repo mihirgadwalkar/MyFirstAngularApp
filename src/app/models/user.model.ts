623ebb0e5ada4526cc150f8a77926fa0db36d0b9
mihir <mihirgadwalkar@gmail.com>
2021-05-09 12:32:53 +0530

    first commit

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,5 @@
+export interface User{
+    username:string;
+    dob:string;
+    email:string;
+}
---
+++ src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model'
+
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css']
+})
+export class UserRegistrationComponent {
+
+  userObj:User={username:"",dob:"",email:""};
+
+  addUserData(){
+    console.log(this.userObj)
+    this.userObj={username:"",dob:"",email:""}
+  }
+
+}
